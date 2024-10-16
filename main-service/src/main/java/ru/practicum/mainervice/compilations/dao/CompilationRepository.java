197703b10628d4c8302bd52e2a78680ea831d53c
IAmA1ex <alexander.mitevitch@yandex.ru>
2024-10-17 00:30:12 +0300

    feature: Public - Подборки событий

--- main-service/src/main/java/ru/practicum/mainervice/compilations/dao/CompilationRepository.java
+++ main-service/src/main/java/ru/practicum/mainervice/compilations/dao/CompilationRepository.java
@@ -1,8 +1,18 @@
 package ru.practicum.mainervice.compilations.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.practicum.mainervice.compilations.model.Compilation;
+
+import java.util.List;
 
 public interface CompilationRepository extends JpaRepository<Compilation, Long> {
 
+    @Query("""
+        SELECT c FROM Compilation c
+        WHERE c.pinned = :pinned
+        ORDER BY c.id ASC
+        LIMIT :size OFFSET :from
+    """)
+    List<Compilation> findAllByPinnedAndSorted(Boolean pinned, Long from, Long size);
 }
